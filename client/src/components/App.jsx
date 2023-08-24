83defcbed32fcfc79254f7997d90695b4d240e94
Danil Simonov <simdanila@gmailcom>
2023-08-25 00:43:47 +0300

    Создал сервер, страницу авторизации

---
+++ client/src/components/App.jsx
@@ -0,0 +1,41 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './navbar/Navbar';
+import Registration from './authorization/Registration';
+import './app.css';
+import Login from './authorization/Login';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { auth } from '../actions/user';
+
+function App() {
+  const isAuth = useSelector((state) => state.user.isAuth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(auth());
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <div className='app'>
+        <Navbar />
+        <div className='wrap'>
+          {!isAuth && (
+            <Routes>
+              <Route
+                path='/registration'
+                element={<Registration />}
+              />
+              <Route
+                path='/login'
+                element={<Login />}
+              />
+            </Routes>
+          )}
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const authRouter = require('./routes/auth.routes');
+const corsMiddleware = require('./middleware/cors.middleware')
+
+const app = express();
+const PORT = config.get('PORT');
+
+app.use(corsMiddleware)
+app.use(express.json());
+
+app.use('/api/auth', authRouter);
+
+const start = async () => {
+  try {
+    mongoose.connect(config.get('dbUrl'));
+
+    app.listen(PORT, () => {
+      console.log('Server started on port: ', PORT);
+    });
+  } catch (err) {}
+};
+start();
