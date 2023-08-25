fd5e52b54fdb7643601d5aefabbd3a9b18239b86
Danil Simonov <simdanila@gmailcom>
2023-08-25 03:01:49 +0300

    Сервер: Модель файла, создание файла, роуты

--- server/index.js
+++ server/index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
 const authRouter = require('./routes/auth.routes');
-const corsMiddleware = require('./middleware/cors.middleware')
+const fileRouter = require('./routes/file.routes');
+const corsMiddleware = require('./middleware/cors.middleware');
 
 const app = express();
 const PORT = config.get('PORT');
 
-app.use(corsMiddleware)
+app.use(corsMiddleware);
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
+app.use('/api/files', fileRouter);
 
 const start = async () => {
   try {
